d8f1bae4fe752496667686141d2c43fd309692ac
Leonardo Martins <34400625+LeoMartinsBDS@users.noreply.github.com>
2020-04-15 16:45:44 -0300

    Upload arquivo correto

---
+++ youtube_clone/lib/api.dart
@@ -0,0 +1,42 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+import 'model/video.dart';
+
+const CHAVE_YOUTUBE_API = "";
+const ID_CANAL = "UCyr0HY0vRPJaVZQNBP36Orw";
+const URL_BASE = "https://www.googleapis.com/youtube/v3/";
+
+class API {
+
+  Future<List<Video>> pesquisar(String pesquisa) async {
+
+    http.Response response = await http.get(
+      URL_BASE + "search"
+            "?part=snippet"
+            "&type=video"
+            "&maxResults=20"
+            "&order=date"
+            "&key=$CHAVE_YOUTUBE_API"
+            "&q=$pesquisa"
+    );
+
+    if(response.statusCode == 200){
+
+      Map<String, dynamic> dadosJson = json.decode(response.body);
+
+      List<Video> videos = dadosJson["items"].map<Video>(
+          (map){
+            return Video.fromJson(map);
+          }
+      ).toList();
+
+    return videos;
+
+    }
+    else{
+
+    }
+  }
+
+}
