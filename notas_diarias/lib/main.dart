9470c59e2045b0079e8a47b73e8aae1f08526111
LeoMartinsBDS <34400625+LeoMartinsBDS@users.noreply.github.com>
2020-04-27 22:11:54 -0300

    App minhas anotações utilizando SQLite

---
+++ notas_diarias/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:notas_diarias/home.dart';
+
+void main() => runApp(
+  MaterialApp(
+    home: Home(),
+    debugShowCheckedModeBanner: false,
+  )
+);
+
+
